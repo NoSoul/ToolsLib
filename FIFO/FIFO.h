ba3a2e2d173ce33bb10a6db485add29d382fdada
NoSoul <NoSoul.Love@GMail.com>
2014-10-13 14:28:02 +0800

    更新FIFO类

--- FIFO/FIFO.h
+++ FIFO/FIFO.h
@@ -2,6 +2,7 @@
 #define _FIFO_H_
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 template <typename FIFOType_t>
 class FIFO
 {
@@ -44,6 +45,7 @@ class FIFO
                 mData = NULL;
             }
             mData = new FIFOType_t[source.mSize];
+            memcpy(mData, source.mData, sizeof(FIFOType_t)*source.mSize);
             assert(mData);
             mReadPos = source.mReadPos;
             mWritePos = source.mWritePos;
@@ -101,6 +103,53 @@ class FIFO
         }
         bool Empty(){return mCount == 0;}
         int GetLength(){return mCount;}
+        FIFOType_t GetAvgValue()
+        {
+            FIFOType_t Sum = 0;
+            for(int curr=mReadPos, i=0; i<mCount; ++i)
+            {
+                Sum += mData[curr];
+                if(++curr == mSize)
+                {
+                    curr = 0;
+                }
+            }
+            return Sum/mCount;
+        }
+        FIFOType_t GetMaxValue()
+        {
+            assert(mCount);
+            FIFOType_t ans = mData[mReadPos];
+            for(int curr=mReadPos, i=0; i<mCount; ++i)
+            {
+                if(mData[curr] > ans)
+                {
+                    ans = mData[curr];
+                }
+                if(++curr == mSize)
+                {
+                    curr = 0;
+                }
+            }
+            return ans;
+        }
+        FIFOType_t GetMinValue()
+        {
+            assert(mCount);
+            FIFOType_t ans = mData[mReadPos];
+            for(int curr=mReadPos, i=0; i<mCount; ++i)
+            {
+                if(mData[curr] < ans)
+                {
+                    ans = mData[curr];
+                }
+                if(++curr == mSize)
+                {
+                    curr = 0;
+                }
+            }
+            return ans;
+        }
     private:
         FIFOType_t *mData;
         int mReadPos;
